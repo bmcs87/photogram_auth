35b7e7501fbfba2cd8abb75d3503f174227bbb2d
Becca Smith <rsmith71@chicagobooth.edu>
2017-08-13 17:35:11 +0000

    1235 pm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+  
+  protect_from_forgery with: :exception
+  
+  before_action :authenticate_user!
+  
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -6,6 +11,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, :keys => [:username])
 
-    devise_parameter_sanitizer.permit(:account_update)
+    devise_parameter_sanitizer.permit(:account_update, :keys => [])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @photos = @user.photos
+    @user.username = params[:username]
     #we can do the @user.photos because we made the helper methods and connections at the beginning
   end
 
